3919fc56d7a4480863bfc87190ec1b04537ac414
Rafa MZ <mz.tears.of.time@gmail.com>
2024-08-15 12:20:50 -0300

    5. Melhorias no modal 'receber destaques'

--- js/dialog.js
+++ js/dialog.js
@@ -4,15 +4,17 @@ var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
+btnAbreDialog.style.display = 'block';
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');
 }
 
 // Listeners
