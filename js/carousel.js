2615ec72a466d740ed2843760f6304d9d6148ee1
Rafa MZ <mz.tears.of.time@gmail.com>
2024-08-15 15:31:10 -0300

    14. Movimentando carrossel com js

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,15 @@
+// Variaveis
+
+var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+
+var new0 = document.querySelector('#new0');
+var new1 = document.querySelector('#new1');
+var new2 = document.querySelector('#new2');
+
+new0.style.display = 'block'
+
+document.querySelector('#new0').style.display = 'block';
+
 var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
 
 // Percorre todos os botoes controladores
@@ -5,6 +17,20 @@ btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
     
 
+	if (this.getAttribute('data-sliderItem') === '0') {
+        new0.style.display = 'block';
+        new1.style.display = 'none';
+        new2.style.display = 'none';
+     } else if (this.getAttribute('data-sliderItem') === '1') {
+        new0.style.display = 'none';
+        new1.style.display = 'block';
+        new2.style.display = 'none';
+     } else {
+        new0.style.display = 'none';
+        new1.style.display = 'none';
+        new2.style.display = 'block';
+     }
+	 
     // Remove classe 'ativo' dos outros botoes
     btns.forEach(function(btnRemoveClass) {
       btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo')
