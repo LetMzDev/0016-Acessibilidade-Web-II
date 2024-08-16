e5ef3c0e3aa9c733438744af6ebdb1f5f4aa54dc
Rafa MZ <mz.tears.of.time@gmail.com>
2024-08-16 13:09:43 -0300

    25. Sugestão de email

--- js/mailcheck-config.js
+++ js/mailcheck-config.js
@@ -1,20 +1,18 @@
+var campoEmail = document.querySelector('#email');
+var sugestao = document.querySelector('#sugestao')
+
 var domains = ['gmail.com', 'aol.com', 'outlook.com'];
 var secondLevelDomains = ['hotmail']
 var topLevelDomains = ["com", "net", "org", 'br'];
 
-var superStringDistance = function (string1, string2) {
-	// a string distance algorithm of your choosing
-}
-
-document.querySelector('#email').addEventListener('blur', function () {
+campoEmail.addEventListener('blur', function () {
 	Mailcheck.run({
-		email: yourTextInput.value,
+		email: campoEmail.value,
 		domains: domains,                       // optional
 		topLevelDomains: topLevelDomains,       // optional
 		secondLevelDomains: secondLevelDomains, // optional
-		distanceFunction: superStringDistance,  // optional
 		suggested: function (suggestion) {
-			// callback code
+			console.log(suggestion.full);
 		}
 	});
 });
